ed330d18a55b197b4c031acd8ec3730cb86e60e0
Ryan Lee <rlee33@gmail.com>
2017-01-16 15:21:49 -0600

    final tweaks

--- ImageBrowser/ImageBrowser/DetailViewController.swift
+++ ImageBrowser/ImageBrowser/DetailViewController.swift
@@ -17,9 +17,21 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = selectedImage
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 
     override func didReceiveMemoryWarning() {
