4fc5da0bfe46cc77b03c4b2d3024d65b201c2ce3
Ryan Lee <rlee33@gmail.com>
2017-01-19 16:19:12 -0600

    change functionality to share on facebook

--- ImageBrowser/ImageBrowser/DetailViewController.swift
+++ ImageBrowser/ImageBrowser/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class DetailViewController: UIViewController {
 
@@ -37,9 +38,15 @@ class DetailViewController: UIViewController {
     }
     
     func shareTapped() {
-        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
-        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-        present(vc, animated: true)
+        if let vc = SLComposeViewController(forServiceType: SLServiceTypeFacebook) {
+            vc.setInitialText("Look at this great picture!")
+            vc.add(imageView.image!)
+            vc.add(URL(string: "http://www.photolib.noaa.gov/nssl"))
+            present(vc, animated: true)
+        }
+//        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+//        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+//        present(vc, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
