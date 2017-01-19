2b4099b2c0efc2bb6704aa784ba96716b22e9b43
Ryan Lee <rlee33@gmail.com>
2017-01-18 23:10:07 -0600

    add style to flags so white colors do not appear transparent

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -19,24 +19,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        countries.append("estonia")
-        countries.append("france")
-        countries.append("germany")
-        countries.append("ireland")
-        countries.append("italy")
-        countries.append("monaco")
-        countries.append("nigeria")
-        countries.append("poland")
-        countries.append("russia")
-        countries.append("spain")
-        countries.append("uk")
-        countries.append("us")
+        // Do any additional setup after loading the view, typically from a nib
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
         
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
