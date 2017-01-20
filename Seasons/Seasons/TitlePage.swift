3a0078fcb3abafc4d72a6eb1a720139fb6fcfc60
Ryan Lee <rlee33@gmail.com>
2017-01-20 15:04:06 -0600

    hide previous and next buttonson title screen, hooked up readButton to go to next scene.

--- Seasons/Seasons/TitlePage.swift
+++ Seasons/Seasons/TitlePage.swift
@@ -9,8 +9,24 @@
 import SpriteKit
 
 class TitlePage: GameScene {
+    
+    var readButton: SKSpriteNode!
 
     override func getNextScene() -> SKScene? {
         return SKScene(fileNamed: "Scene01") as! Scene01
+    }
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+        readButton = childNode(withName: "readButton") as! SKSpriteNode
+        
+        btnNext.isHidden = true
+        btnPrevious.isHidden = true
+    }
+    
+    override func touchDown(at point: CGPoint) {
+        if readButton.contains(point) {
+            goToScene(scene: getNextScene()!)
+        }
     }
 }
