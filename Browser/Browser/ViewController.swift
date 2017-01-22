99e16df7094c2f8c7b1227e72018c56dcf0f55b0
Ryan Lee <rlee33@gmail.com>
2017-01-21 21:03:39 -0600

    add webView and WKNavigationDelegate

--- Browser/Browser/ViewController.swift
+++ Browser/Browser/ViewController.swift
@@ -6,13 +6,25 @@
 //  Copyright © 2017 Ryan Lee. All rights reserved.
 //
 
+import WebKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
     override func didReceiveMemoryWarning() {
