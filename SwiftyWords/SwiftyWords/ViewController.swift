4ced8f30f5b7142e19a006633ff47082abb33cbd
Ryan Lee <rlee33@gmail.com>
2017-02-01 11:35:27 -0600

    add logic for dissecting .txt file into answers and clues.

--- SwiftyWords/SwiftyWords/ViewController.swift
+++ SwiftyWords/SwiftyWords/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Ryan Lee. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -21,11 +22,56 @@ class ViewController: UIViewController {
     @IBAction func clearTapped(_ sender: Any) {
     }
     
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
+    }
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePath) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+                
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.characters.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+        // configure buttons and labels
+    }
+    
+    func letterTapped(btn: UIButton) {
+        
     }
 
     override func didReceiveMemoryWarning() {
