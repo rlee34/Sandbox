4408200923935925de7961da1cc7c7888e6f250a
Ryan Lee <rlee33@gmail.com>
2017-02-03 11:09:36 -0600

    init remake

---
+++ WhitehousePetitionsRemake/WhitehousePetitionsRemake/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  WhitehousePetitionsRemake
+//
+//  Created by Ryan Lee on 2/2/17.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+}
+
