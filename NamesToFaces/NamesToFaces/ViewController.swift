d43c64fece2d3821014540580a51b73a0ea26eed
Ryan Lee <rlee33@gmail.com>
2017-02-09 16:23:49 -0600

    add left navbar button to add new person, updated plist with photo library usage definition

--- NamesToFaces/NamesToFaces/ViewController.swift
+++ NamesToFaces/NamesToFaces/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+    }
+    
+    func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
