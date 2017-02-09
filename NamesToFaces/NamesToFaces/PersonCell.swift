3b8577a05dcfb96f9f6de1e0bfac25c61dd789ce
Ryan Lee <rlee33@gmail.com>
2017-02-09 11:52:09 -0600

    add class to represent cell called PersonCell, made connections for image and name label

---
+++ NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Ryan Lee on 2/9/17.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
