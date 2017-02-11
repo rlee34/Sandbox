7425a463a615c3c4afc1a1493944f53d03eea495
Ryan Lee <rlee33@gmail.com>
2017-02-11 15:06:09 -0600

    fix bouncer so it's static. add bouncers to scene

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -18,7 +18,11 @@ class GameScene: SKScene {
         addChild(background)
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
-        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -37,6 +41,7 @@ class GameScene: SKScene {
         bouncer.position = position
         bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
         bouncer.physicsBody!.restitution = 0.3
+        bouncer.physicsBody?.isDynamic = false
         addChild(bouncer)
     }
 }
