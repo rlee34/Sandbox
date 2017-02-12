16fc201159c31c105a2fc562aa40fbfd943a1fa3
Ryan Lee <rlee33@gmail.com>
2017-02-12 17:48:49 -0600

    add physics body to slot base

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -64,6 +64,10 @@ class GameScene: SKScene {
         
         slotBase.position = position
         slotGlow.position = position
+        
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody!.isDynamic = false
+        
         addChild(slotBase)
         addChild(slotGlow)
         
