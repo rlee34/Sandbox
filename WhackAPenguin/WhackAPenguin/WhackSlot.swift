753712d1e90bf78f802cf3bf81782bd0b6eef6dc
Ryan Lee <rlee33@gmail.com>
2017-02-18 18:45:39 -0600

    add WhackSlot class and func for creating hole sprites

---
+++ WhackAPenguin/WhackAPenguin/WhackSlot.swift
@@ -0,0 +1,21 @@
+//
+//  WhackSlot.swift
+//  WhackAPenguin
+//
+//  Created by Ryan Lee on 2/18/17.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+    }
+}
