2301f60832649dbcefdc18aaff88ddcfb6b14130
Ryan Lee <rlee33@gmail.com>
2017-02-19 19:08:28 -0600

    add crop node

--- WhackAPenguin/WhackAPenguin/WhackSlot.swift
+++ WhackAPenguin/WhackAPenguin/WhackSlot.swift
@@ -11,10 +11,24 @@ import UIKit
 
 class WhackSlot: SKNode {
     
+    var charNode: SKSpriteNode!
+    
     func configure(at position: CGPoint) {
         self.position = position
         
         let sprite = SKSpriteNode(imageNamed: "whackHole")
         addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = nil
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
     }
 }
