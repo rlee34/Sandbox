b1c112800cb3011009cd230921ab93c4695a2d77
Ryan Lee <rlee33@gmail.com>
2017-02-19 18:54:21 -0600

    add loops to create slots

--- WhackAPenguin/WhackAPenguin/GameScene.swift
+++ WhackAPenguin/WhackAPenguin/GameScene.swift
@@ -32,6 +32,11 @@ class GameScene: SKScene {
         gameScore.horizontalAlignmentMode = .left
         gameScore.fontSize = 48
         addChild(gameScore)
+        
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410)) }
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
