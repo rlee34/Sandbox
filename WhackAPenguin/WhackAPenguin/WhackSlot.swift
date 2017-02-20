34ee8f58678883d4363b9ac4d48a10b72fdb0ea6
Ryan Lee <rlee33@gmail.com>
2017-02-20 07:45:20 -0600

    add hit method

--- WhackAPenguin/WhackAPenguin/WhackSlot.swift
+++ WhackAPenguin/WhackAPenguin/WhackSlot.swift
@@ -60,4 +60,13 @@ class WhackSlot: SKNode {
         charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
         isVisible = false
     }
+    
+    func hit() {
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [unowned self] in self.isVisible = false }
+        charNode.run(SKAction.sequence([delay, hide, notVisible]))
+    }
 }
