5a7e29aa79c54d5ad6911a8a4c631db58a9e1427
Ryan Lee <rlee33@gmail.com>
2017-02-22 14:21:06 -0600

    add logic to cycle through animations

--- Animation/Animation/ViewController.swift
+++ Animation/Animation/ViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var Tap: NSLayoutConstraint!
     
+    
+    @IBOutlet weak var tap: UIButton!
     @IBAction func tapped(_ sender: Any) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
     }
+
+    var imageView: UIImageView!
+    var currentAnimation = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
