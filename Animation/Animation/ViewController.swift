dcec585835b1e95b3e111c0446dafd44a0fdac43
Ryan Lee <rlee33@gmail.com>
2017-02-22 13:58:54 -0600

    add outlets

--- Animation/Animation/ViewController.swift
+++ Animation/Animation/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Tap: NSLayoutConstraint!
+    
+    @IBAction func tapped(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
