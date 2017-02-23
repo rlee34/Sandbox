729d546b32da4e33602804c71bdd2ac4069b9991
Ryan Lee <rlee33@gmail.com>
2017-02-22 19:47:39 -0600

    add logic to unwrap js dictionary

--- Javascript Injection/Extension/ActionViewController.swift
+++ Javascript Injection/Extension/ActionViewController.swift
@@ -19,7 +19,9 @@ class ActionViewController: UIViewController {
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error ) in
-                    // do stuff!
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    print(javaScriptValues)
                 }
             }
         }
