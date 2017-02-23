28bc29101524827fbee655f6f4766c6883528ee6
Ryan Lee <rlee33@gmail.com>
2017-02-23 11:50:24 -0600

    add background and set physics for world

--- Swifty Ninja/Swifty Ninja/GameScene.swift
+++ Swifty Ninja/Swifty Ninja/GameScene.swift
@@ -13,7 +13,15 @@ class GameScene: SKScene {
 
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+     
     }
     
     
