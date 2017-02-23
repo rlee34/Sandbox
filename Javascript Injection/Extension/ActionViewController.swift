d0c5d0c109436489146c3a447fec504d1758a6f0
Ryan Lee <rlee33@gmail.com>
2017-02-22 21:08:21 -0600

    implement done action

--- Javascript Injection/Extension/ActionViewController.swift
+++ Javascript Injection/Extension/ActionViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 import MobileCoreServices
 
 class ActionViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
-
+    
+    var pageTitle = ""
+    var pageURL = ""
+    
+    @IBOutlet var script: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
         
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error ) in
                     let itemDictionary = dict as! NSDictionary
                     let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
-                    print(javaScriptValues)
+                    
+                    self.pageTitle = javaScriptValues["title"] as! String
+                    self.pageURL = javaScriptValues["URL"] as! String
+                    
+                    DispatchQueue.main.async {
+                        self.title = self.pageTitle
+                    }
                 }
             }
         }
+     
+        
+    }
+    
+    @IBAction func done() {
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = ["NSExtensionJavaScriptFinalizeArgumentKey": argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        
+        extensionContext!.completeRequest(returningItems: [item])
     }
     
 }
