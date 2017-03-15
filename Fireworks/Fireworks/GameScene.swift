58c9aaabdee7751902298147e4ef30c3acb9a2a7
Ryan Lee <rlee33@gmail.com>
2017-03-14 21:31:38 -0500

    add method for firework creation

--- Fireworks/Fireworks/GameScene.swift
+++ Fireworks/Fireworks/GameScene.swift
@@ -34,6 +34,43 @@ class GameScene: SKScene {
         gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
     }
     
+    func createFirework(xMovement: CGFloat, x: Int, y: Int) {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch GKRandomSource.sharedRandom().nextInt(upperBound: 3) {
+        case 0:
+            firework.color = .cyan
+            firework.colorBlendFactor = 1
+        case 1:
+            firework.color = .green
+            firework.colorBlendFactor = 1
+        case 2:
+            firework.color = .red
+            firework.colorBlendFactor = 1
+        default:
+            break
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+        
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        node.run(move)
+        
+        let emitter = SKEmitterNode(fileNamed: "fuse")!
+        emitter.position = CGPoint(x: 0, y: -22)
+        node.addChild(emitter)
+        
+        fireworks.append(node)
+        addChild(node)
+    }
+    
     
 
 
