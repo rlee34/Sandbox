3ee030eaa6821b068cd0de1dc1614f28830854a9
Ryan Lee <rlee33@gmail.com>
2017-03-16 17:32:21 -0500

    add implementation for registerLocal and scheduleLocal methods

--- Local Notifications/Local Notifications/ViewController.swift
+++ Local Notifications/Local Notifications/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Ryan Lee. All rights reserved.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
@@ -22,11 +23,35 @@ class ViewController: UIViewController {
     }
 
     func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
         
     }
     
     func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
         
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird cates the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default()
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
 }
 
