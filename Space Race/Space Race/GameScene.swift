6af12ab8cde9c7d5c0263faf91316546dbd86e74
Ryan Lee <rlee33@gmail.com>
2017-03-21 20:47:06 -0500

    init

---
+++ Space Race/Space Race/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Space Race
+//
+//  Created by Ryan Lee on 3/21/17.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+   
+}
