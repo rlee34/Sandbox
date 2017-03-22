694f74ba553ad54f9ec94b6ddf5b7d8e2d7c9ef9
Ryan Lee <rlee33@gmail.com>
2017-03-21 20:57:48 -0500

    add player and score label

--- Space Race/Space Race/GameScene.swift
+++ Space Race/Space Race/GameScene.swift
@@ -29,6 +29,19 @@ class GameScene: SKScene {
         starfield.advanceSimulationTime(10)
         addChild(starfield)
         starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x:100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody!.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
+        score = 0
     }
    
 }
