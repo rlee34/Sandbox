2c775f53f9ce2c0751eb59859d0a0ed5f25f5f28
Ryan Lee <rlee33@gmail.com>
2017-03-24 21:20:29 -0500

    add collectionView method

--- Selfie Share/Selfie Share/ViewController.swift
+++ Selfie Share/Selfie Share/ViewController.swift
@@ -27,5 +27,15 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
 }
 
