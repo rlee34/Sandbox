5a1deb4b3900d6b08a7fce55a1c92633b94184d2
Ryan Lee <rlee33@gmail.com>
2017-03-26 22:00:43 -0500

    add method stubs required by MCSessionDelegate and MCBrowserViewControllerDelegate

--- Selfie Share/Selfie Share/ViewController.swift
+++ Selfie Share/Selfie Share/ViewController.swift
@@ -82,5 +82,25 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         return cell
     }
     
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
 }
 
