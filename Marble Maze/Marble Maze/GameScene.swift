8b7bebcdebc8d2cd3cd2c6def895df489d050ce7
Ryan Lee <rlee33@gmail.com>
2017-03-28 21:33:27 -0500

    modify loadLevel to create block tile for the x character

--- Marble Maze/Marble Maze/GameScene.swift
+++ Marble Maze/Marble Maze/GameScene.swift
@@ -33,7 +33,13 @@ class GameScene: SKScene {
                         let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
                         
                         if letter == "x" {
-                            // load wall
+                            let node = SKSpriteNode(imageNamed: "block")
+                            node.position = position
+                            
+                            node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                            node.physicsBody!.categoryBitMask = CollisionTypes.wall.rawValue
+                            node.physicsBody!.isDynamic = false
+                            addChild(node)
                         } else if letter == "v" {
                             // load vortex
                         } else if letter == "s" {
