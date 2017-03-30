1ad168f91050323d631b477c3f41b143f38679a3
Ryan Lee <rlee33@gmail.com>
2017-03-30 00:59:23 -0500

    add #if/#else to check what device is in running the app and choose the appropriate logic to detect motion.

--- Marble Maze/Marble Maze/GameScene.swift
+++ Marble Maze/Marble Maze/GameScene.swift
@@ -40,11 +40,16 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
+    #if (arch(i386) || arch(x86_64))
         if let currentTouch = lastTouchPosition {
             let diff = CGPoint(x: currentTouch.x - player.position.x, y: currentTouch.y - player.position.y)
             physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
         }
-        
+    #else
+        if let accelerometerData = motionManager.accelerometerData {
+            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * -50, dy: accelerometerData.acceleration.x * 50)
+        }
+    #endif
     }
     
     func createPlayer() {
