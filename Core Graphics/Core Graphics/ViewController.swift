8d374fac283dfabc70f9e5ac0c8e8ae3057f0463
Ryan Lee <rlee33@gmail.com>
2017-04-02 22:13:40 -0500

    add initial layout and constraints, created outlets for image view and redraw button

--- Core Graphics/Core Graphics/ViewController.swift
+++ Core Graphics/Core Graphics/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func redrawTapped(_ sender: Any) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
