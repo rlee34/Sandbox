d4f8671ee3e5c41e28f1d96b04c8d891804108b2
Ryan Lee <rlee33@gmail.com>
2017-04-04 20:17:48 -0500

    add drawCircle method

--- Core Graphics/Core Graphics/ViewController.swift
+++ Core Graphics/Core Graphics/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
+        case 1:
+            drawCircle()
         default:
             break
         }
@@ -56,5 +58,22 @@ class ViewController: UIViewController {
         imageView.image = img
     }
 
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512)
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = img
+    }
+        
 }
 
