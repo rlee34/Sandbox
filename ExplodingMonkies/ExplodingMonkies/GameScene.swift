5ab03cd4484766b66a785b0404343b0dc874f195
Ryan Lee <rlee33@gmail.com>
2017-04-07 15:40:12 -0500

    add createBuildings method to randomly generate buildings to the game scene

--- ExplodingMonkies/ExplodingMonkies/GameScene.swift
+++ ExplodingMonkies/ExplodingMonkies/GameScene.swift
@@ -17,6 +17,27 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
     
+    var buildings = [BuildingNode]()
+    
     override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: RandomInt(min: 2, max: 4) * 40, height: RandomInt(min: 300, max: 600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
     }
 }
