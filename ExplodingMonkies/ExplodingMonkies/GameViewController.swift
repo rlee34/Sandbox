b2a8077fe3964135c48bb975994b1d481b7845eb
Ryan Lee <rlee33@gmail.com>
2017-04-09 21:02:31 -0500

    add implementation of launch action

--- ExplodingMonkies/ExplodingMonkies/GameViewController.swift
+++ ExplodingMonkies/ExplodingMonkies/GameViewController.swift
@@ -30,6 +30,15 @@ class GameViewController: UIViewController {
     }
     
     @IBAction func launch(_ sender: Any) {
+        angleSlider.isHidden = true
+        angleLabel.isHidden = true
+        
+        velocitySlider.isHidden = true
+        velocityLabel.isHidden = true
+        
+        launchButton.isHidden = true
+        
+        currentGame.launch(angle: Int(angleSlider.value), velocity: Int(velocitySlider.value))
     }
     
     
