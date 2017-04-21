0c346a054c7ee780fec32fe14784df56462e90f3
Ryan Lee <rlee33@gmail.com>
2017-04-20 23:01:24 -0500

    add outlets and set viewcontroller to be delegate of addressBar

--- MultiBrowser/MultiBrowser/ViewController.swift
+++ MultiBrowser/MultiBrowser/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
