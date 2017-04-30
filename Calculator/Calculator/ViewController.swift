57a5fa1ad5ad39142bd89aa28f0740817e2d0d1b
Ryan Lee <rlee33@gmail.com>
2017-04-29 22:16:22 -0500

    add button for add and equals operators and created method for adding numbers and implemented equal operator as a switch

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
+enum Operator {
+    case add
+    case subtract
+    case multiply
+    case divide
+    case inital
+}
+
 class ViewController: UIViewController {
     
     var userIsCurrentlyTyping = false
+    var leftHandNumber: String?
+    var rightHandNumber: String?
+    var operation: Operator = .inital
+
     
     @IBOutlet weak var calculatorWindow: UILabel!
     
@@ -26,6 +38,48 @@ class ViewController: UIViewController {
         
         userIsCurrentlyTyping = true
     }
+    
+    @IBAction func touchPi(_ sender: UIButton) {
+        calculatorWindow.text = String(M_PI)
+        userIsCurrentlyTyping = false
+    }
+    
+    @IBAction func touchAddButton(_ sender: UIButton) {
+        operation = Operator.add
+        leftHandNumber = calculatorWindow.text
+        calculatorWindow.text = "0"
+        userIsCurrentlyTyping = false
+    }
+    
+    @IBAction func touchEqualOperator(_ sender: UIButton) {
+        rightHandNumber = calculatorWindow.text
+        
+        switch operation {
+        case .add:
+            if let leftHandNumber = leftHandNumber {
+                addCurrentWith(leftNumber: leftHandNumber)
+            }
+        case .subtract:
+            break
+        case .multiply:
+            break
+        case .divide:
+            break
+        case .inital:
+            break
+        }
+        
+    }
+    
+    func addCurrentWith(leftNumber: String) {
+        if let rightNumber = rightHandNumber {
+            let sum = Double(leftNumber)! + Double(rightNumber)!
+            calculatorWindow.text = String(sum)
+            leftHandNumber = String(sum)
+            rightHandNumber = nil
+        }
+    }
+    
     
 }
 
