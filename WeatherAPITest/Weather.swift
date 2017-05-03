5dc4eac6325ca2a4a59c1095ecde142419b092d6
Ryan Lee <rlee33@gmail.com>
2017-05-02 23:00:40 -0500

    add initializer for Weather struct

--- WeatherAPITest/Weather.swift
+++ WeatherAPITest/Weather.swift
@@ -37,4 +37,49 @@ struct Weather {
     let pressure: Int
     let cloudCover: Int
     let windSpeed: Double
+    
+    let windDirection: Double?
+    let rainfallInLast3Hours: Double?
+    
+    let sunrise: NSDate
+    let sunset: NSDate
+    
+    init(weatherData: [String: AnyObject]) {
+        dateAndTime = NSDate(timeIntervalSince1970: weatherData["dt"] as! TimeInterval)
+        city = weatherData["name"] as! String
+        
+        let coordDict = weatherData["coord"] as! [String: AnyObject]
+        longitude = coordDict["lon"] as! Double
+        latitude = coordDict["lat"] as! Double
+        
+        let weatherDict = weatherData["weather"]![0] as! [String: AnyObject]
+        weatherID = weatherDict["id"] as! Int
+        weatherDescription = weatherDict["description"] as! String
+        mainWeather = weatherDict["main"] as! String
+        weatherIconID = weatherDict["icon"] as! String
+        
+        let mainDict = weatherData["main"] as! [String: AnyObject]
+        temp = mainDict["temp"] as! Double
+        humidity = mainDict["humidity"] as! Int
+        pressure = mainDict["pressure"] as! Int
+        
+        cloudCover = weatherData["clouds"]!["all"] as! Int
+        
+        let windDict = weatherData["wind"] as! [String: AnyObject]
+        windSpeed = windDict["speed"] as! Double
+        windDirection = windDict["deg"] as? Double //
+        
+        if weatherData["rain"] != nil {
+            let rainDict = weatherData["rain"] as! [String: AnyObject]
+            rainfallInLast3Hours = rainDict["3h"] as? Double
+        } else {
+            rainfallInLast3Hours = nil
+        }
+        
+        let sysDict = weatherData["sys"] as! [String: AnyObject]
+        country = sysDict["country"] as! String
+        sunrise = NSDate(timeIntervalSince1970: sysDict["sunrise"] as! TimeInterval)
+        sunset = NSDate(timeIntervalSince1970: sysDict["sunset"] as! TimeInterval)
+        
+    }
 }
