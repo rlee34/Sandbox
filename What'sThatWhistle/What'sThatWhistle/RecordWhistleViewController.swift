4a052815939198af08b5a3e693af19c34da9ca3c
Ryan Lee <rlee33@gmail.com>
2017-05-07 22:35:52 -0500

    add new UIViewController

---
+++ What'sThatWhistle/What'sThatWhistle/RecordWhistleViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RecordWhistleViewController.swift
+//  What'sThatWhistle
+//
+//  Created by Ryan Lee on 5/7/17.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+import UIKit
+
+class RecordWhistleViewController: UIViewController {
+    
+    var stackView: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.gray
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
