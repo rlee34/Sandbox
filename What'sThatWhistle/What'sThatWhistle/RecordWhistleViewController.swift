9b3e36835886503df882a878b804acc63f36869b
Ryan Lee <rlee33@gmail.com>
2017-05-09 16:16:53 -0500

    add two class methods for getting document directory

--- What'sThatWhistle/What'sThatWhistle/RecordWhistleViewController.swift
+++ What'sThatWhistle/What'sThatWhistle/RecordWhistleViewController.swift
@@ -78,7 +78,15 @@ class RecordWhistleViewController: UIViewController {
         stackView.addArrangedSubview(failLabel)
     }
     
-
+    class func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    class func getWhistleURL() -> URL {
+        return getDocumentsDirectory().appendingPathComponent("whistle.m4a")
+    }
     /*
     // MARK: - Navigation
 
