0606c849298a4f4c5a436196d492b5a42e933559
Ryan Lee <rlee33@gmail.com>
2017-05-17 21:33:37 -0500

    add stored property to return UIColor dependant on which theme is selected.

--- Pet Finder/Pet Finder/Theme.swift
+++ Pet Finder/Pet Finder/Theme.swift
@@ -19,4 +19,16 @@ enum Theme: Int {
     let storedTheme = UserDefaults.standard.integer(forKey: Keys.selectedTheme)
     return Theme(rawValue: storedTheme) ?? .default
   }
+  
+  var mainColor: UIColor {
+    switch self {
+    case .default:
+      return UIColor(red: 87.0/255.0, green: 188.0/255.0, blue: 95.0/255.0, alpha: 1.0)
+    case .dark:
+      return UIColor(red: 255.0/255.0, green: 115.0/255.0, blue: 50.0/255.0, alpha: 1.0)
+    case .graphical:
+      return UIColor(red: 10.0/255.0, green: 10.0/255.0, blue: 10.0/255.0, alpha: 1.0)
+    }
+  }
+  
 }
