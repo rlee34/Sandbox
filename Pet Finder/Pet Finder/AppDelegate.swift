e8afb80ab201f5d1a317ad29c834d279e602d7df
Ryan Lee <rlee33@gmail.com>
2017-05-18 22:47:33 -0500

    call Theme.apply() in app delegate to change app to user's preferred theme

--- Pet Finder/Pet Finder/AppDelegate.swift
+++ Pet Finder/Pet Finder/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    print(Theme.current.mainColor)
+    Theme.current.apply()
     return true
   }
 }
