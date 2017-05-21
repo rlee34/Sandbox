ed9e388bde1c00635d4cefa64a72b4234d849726
Ryan Lee <rlee33@gmail.com>
2017-05-20 21:40:20 -0500

    add logic to set image and transition mask image to be used as the back indicator:

--- Pet Finder/Pet Finder/Theme.swift
+++ Pet Finder/Pet Finder/Theme.swift
@@ -52,5 +52,8 @@ enum Theme: Int {
     
     UINavigationBar.appearance().barStyle = barStyle
     UINavigationBar.appearance().setBackgroundImage(navigationBackgroundImage, for: .default)
+    
+    UINavigationBar.appearance().backIndicatorImage = UIImage(named: "backArrow")
+    UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "backArrowMask")
   }
 }
